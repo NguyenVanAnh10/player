291f83ab7d0bd8a52fe93b5343313dfae9858180
nguyenanh <vananh.nguyen1110@gmail.com>
2021-03-23 23:41:13 +0700

    init project

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static("public"));
 // );
 app.get("/", function (req, res) {
   // res.set("Content-Type", "text/html");
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/frontend.html"));
 });
 
 app.listen(port, () => {
