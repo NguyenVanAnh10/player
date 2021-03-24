1b1721c9951a0281ffb36d19024249bc3b50141f
nguyenanh <vananh.nguyen1110@gmail.com>
2021-03-24 08:43:52 +0700

    Removed node_module folder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 const hostname = "0.0.0.0";
-const port = 9000;
+const port = 5000;
 // const config = {
 //   zing: "https://zingmp3.vn",
 //   origin: "http://0.0.0.0:9001",
@@ -15,11 +15,15 @@ app.use(express.static("public"));
 //   "/api/v2/playlist/getDetail",
 //   createProxyMiddleware({ target: config.zing, changeOrigin: true })
 // );
+if (process.env.NODE_ENV === "production") {
+  console.log("xxx");
+}
+
 app.get("/", function (req, res) {
   // res.set("Content-Type", "text/html");
   res.sendFile(path.join(__dirname + "/frontend.html"));
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://${hostname}:${port}`);
 });
